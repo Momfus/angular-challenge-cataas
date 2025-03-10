b6be17b1ad63fda3981d721a27b67e0a2f59e085
Momfus <momfus@outlook.com>
2025-03-09 21:54:42 -0300

    vote list page done

--- src/app/cats/pages/cat-votes-page/cat-votes-page.component.ts
+++ src/app/cats/pages/cat-votes-page/cat-votes-page.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
+import { CatsService } from '../../services/cats.service';
+import { Cat } from '../../interfaces/cats.interface';
+import { CatImageCardComponent } from "../../components/cat-image/cat-image.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cat-votes-page',
-  imports: [],
+  imports: [CatImageCardComponent],
   templateUrl: './cat-votes-page.component.html',
 })
-export default class CatVotesPageComponent { }
+export default class CatVotesPageComponent {
+
+
+  catsService = inject(CatsService);
+  private router = inject(Router);
+
+  visibleCats = signal<Cat[]>([]);
+
+
+  constructor() {
+
+    // Effect to update the visible cats when the cat vote list changes
+    effect(() => {
+      this.visibleCats.set( this.catsService.catVoteList() );
+    });
+
+  }
+
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+}
